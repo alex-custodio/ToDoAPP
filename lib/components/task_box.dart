694d6c15ec215e76e81dac9db11e76a7c869ef11
alexcstd <alex.scustod@gmail.com>
2023-01-01 22:31:23 -0300

    deixando checkbox mais bonita

--- lib/components/task_box.dart
+++ lib/components/task_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class TaskBox extends StatelessWidget {
   String taskName;
   bool? checkMarked;
@@ -17,32 +18,32 @@ class TaskBox extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
       child: Container(
-        padding: EdgeInsets.all(25),
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 72, 64, 118),
+            borderRadius: BorderRadius.circular(5)),
         child: Row(
           children: [
             Checkbox(
               value: checkMarked,
               onChanged: onChanged,
-              activeColor: Color.fromARGB(255, 240, 190, 82),
+              activeColor: const Color.fromARGB(255, 178, 168, 255),
               side: MaterialStateBorderSide.resolveWith(
-                  (states) => BorderSide(width: 1.5, color: Color.fromARGB(255, 240, 190, 82)),
+                  (states) => const BorderSide(width: 1.5, color: Color.fromARGB(255, 178, 168, 255)),
               ),
             ),
             Text(
               taskName,
               style: TextStyle(
-                  color: Colors.white,
+                  color: checkMarked! ? Colors.grey : Colors.white,
                   decoration: checkMarked!
                       ? TextDecoration.lineThrough
                       : TextDecoration.none,
-                  decorationColor: Color.fromARGB(255, 240, 190, 82),
+                  decorationColor: Color.fromARGB(255, 178, 168, 255),
                   decorationThickness: 5),
             )
           ],
         ),
-        decoration: BoxDecoration(
-            color: Color.fromARGB(255, 72, 64, 118),
-            borderRadius: BorderRadius.circular(5)),
       ),
     );
   }
